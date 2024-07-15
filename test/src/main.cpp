b369b0d94c8e28952b4ebbef6f752e163d3a36ab
root <2057479591@qq.com>
2024-07-15 16:39:20 +0800

    1. 调整protocol.h文件的结构体定义位置，使之容易阅读； 2. 添加获取词语列表的api和json定义； 3. 对测试和cmake进行调整；

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,39 +1,90 @@
 ﻿#include <iostream>
 #include <thread>
+#include <fstream>
 #include <lsp/client.h>
+using namespace nlohmann;
+
+bool readFile(std::string& path, std::string* data) 
+{
+    std::ifstream ifStream(path, std::ios::binary | std::ios::in);
+    if (ifStream.is_open()) {
+        ifStream.seekg(0, std::ios::end);
+        int len = ifStream.tellg();
+        ifStream.seekg(0, std::ios::beg);
+        char* buff = new char[len];
+        ifStream.read(buff, len);
+        data->append(buff);
+        delete[]buff;
+    }
+    else {
+        std::cout << "file open fail!!!" << std::endl;
+        return false;
+    }
+    return true;
+}
 
 int main() {
-    URI uri;
-    uri.parse("https://www.baidu.com/test/asdf");
-    printf("%s", uri.host());
-    printf("%s", uri.path());
+    std::string file1_uri = "file:///D:/c_workstation/projects/Third_Fork_Projects/lsp-cpp/test/src/main.cpp";
+    std::string file1_path = "D:/c_workstation/projects/Third_Fork_Projects/lsp-cpp/test/src/main.cpp";
+    std::string root_uri = "file:///D:/c_workstation/projects/Third_Fork_Projects/lsp-cpp";
+    //std::string file1_text;
+    //std::string file2_text;
+    ProcessLanguageClient client(R"(D:\c_workstation\soft_tool\clangd_17.0.3\bin\clangd.exe)");
+    MapMessageHandler msgHandler;
+    auto debugFunc = [](value& j) {
+        //value method = j.at("method");
+        std::cout << "========================================\n";
+        std::cout << j << std::endl;
+    };
+    msgHandler.bindNotify(METHOD_PublishDiagnostics.c_str(), debugFunc);
+    msgHandler.bindNotify(METHOD_DidOpen.c_str(), debugFunc);
+    msgHandler.bindNotify(METHOD_DidClose.c_str(), debugFunc);
 
-    return 0;
-    ProcessLanguageClient client(R"(F:\LLVM\bin\clangd.exe)");
-    MapMessageHandler my;
     std::thread thread([&] {
-        client.loop(my);
+        client.loop(msgHandler);
     });
 
-    string_ref file = "file:///C:/Users/Administrator/Desktop/test.c";
-
-    std::string text = "int main() { return 0; }\n";
     int res;
     while (scanf("%d", &res)) {
-        if (res == 1) {
+        switch (res)
+        {
+        case 0: // 退出
+        {
             client.Exit();
             thread.detach();
             return 0;
+            break;
         }
-        if (res == 2) {
-            client.Initialize();
+        case 1: // 初始化
+        {
+            msgHandler.bindResponse(client.Initialize(string_ref(root_uri)), debugFunc);
+            break;
         }
-        if (res == 3) {
-            client.DidOpen(file, text);
-            client.Sync();
+        case 2: // 打开文件
+        {
+            std::string text;
+            readFile(file1_path, &text);
+            client.DidOpen(file1_uri, text);
+            break;
         }
-        if (res == 4) {
-            client.Formatting(file);
+        case 3: // 关闭文件
+        {
+            client.DidClose(string_ref(file1_uri));
+            break;
+        }
+        case 4: // 获取词语列表
+        {
+            msgHandler.bindResponse(client.SemanticTokensALL(string_ref(file1_uri)), debugFunc);
+            break;
+        }
+        case 5: // 悬停信息
+        {
+            Position pos{8, 13};
+            msgHandler.bindResponse(client.Hover(string_ref(file1_uri), pos), debugFunc);
+            break;
+        }
+        default:
+            break;
         }
     }
     return 0;
